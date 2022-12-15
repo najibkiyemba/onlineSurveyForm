7dc9f5b5a0860245962e4ee4538c4ea7241e9999
Najib Kaur <najibkaur@Najibs-MacBook-Pro.local>
2022-12-15 13:34:32 -0800

    improved the appearance of the site with css and jsx

--- src/components/personalData/UserData.jsx
+++ src/components/personalData/UserData.jsx
@@ -5,50 +5,87 @@ import './userData.css';
 
 const UserData = () => {
     return (
-        <div>
-            <form id="userData">
-                <div className="inputData">
+        <div className="container">
+            <form id="survey-form">
+                <div className="form-group">
                     <label
-                    className="name">Name</label><br/>
+                      id="name-label"
+                      for="name">Name</label><br/>
                     <input
-                    className="inputName"
-                    required type="text"/><br/>
-                    <label
-                    className="email">Email</label><br/>
-                    <input
-                    input="inputEmail"
-                    required type="email"/><br/>
-                    <label
-                    className="age">Age(optional)</label><br/>
-                    <input 
-                    className="inputAge"
-                    required
-                    type="number"/>
+                      placeholder="Enter your name"
+                      className="form-control"
+                      id="name"
+                      name="name"
+                      required
+                      type="text"/><br/>
                 </div>
-                <div>
+                <div className="form-group">
+                    <label
+                      id="email-label"
+                      for="email">Email</label><br/>
+                    <input
+                      placeholder="Enter your email"
+                      name="email"
+                      className="form-control"
+                      required
+                      type="email"/><br/>
+                </div>
+                <div className="form-group">
+                    <label
+                      for="number">Age(optional)</label><br/>
+                    <input
+                      placeholder="Age"
+                      className="form-control"
+                      max="99"
+                      min="10"
+                      id="number"
+                      name="age"
+                      required
+                      type="number"/>
+                </div>
+                
+                <div className="form-group">
                     <p>Which option best describes your current role?</p>
-                    <select>
-                        <option>Select current role</option>
-                        <option>Student</option>
-                        <option>Full Time Job</option>
-                        <option>Full Time Learner</option>
-                        <option>Prefer not to say</option>
-                        <option>Other</option>
+                    <select
+                      required
+                      className="form-control"
+                      id="dropdown"
+                      name="role"
+                      >
+                        <option disabled selected value>Select current role</option>
+                        <option value="student">Student</option>
+                        <option value="job">Full Time Job</option>
+                        <option value="learner">Full Time Learner</option>
+                        <option value="preferNo">Prefer not to say</option>
+                        <option value="other">Other</option>
                     </select>
                 </div>
-                <div>
+                <div className="form-group">
                     <p>Would you recommend online teaching to a friend.</p>
                     <label>
-                        <input name="user" type="radio"/> Definitely
+                        <input className="input-radio" value="definitely" name="user" type="radio"/> Definitely
                     </label><br/>
                     <label>
-                        <input name="user" type="radio"/> Maybe
+                        <input className="input-radio" value="maybe" name="user" type="radio"/> Maybe
                     </label><br/>
                     <label>
-                        <input name="user" type="radio"/> Not sure
+                        <input className="input-radio" value="not-sure" name="user" type="radio"/> Not sure
                     </label>
                 </div>
+                <div className="form-group">
+                    <p>What do you like most during online education?</p>
+                    <select className="form-control" id="most-like" name="mostLike" required>
+                        <option value selected disabled>Select an option</option>
+                        <option value="challenges">Challenges</option>
+                        <option value="projects">Projects</option>
+                        <option value="community">Community</option>
+                        <option value="openSource">Open Source</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <Optional/>
             </form>
+        
         </div>
     );
 };
